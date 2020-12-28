5aa2b1e367fcf10955d326089dc135278d77c5e8
Bjornj95 <48913170+Bjornj95@users.noreply.github.com>
2020-12-28 11:47:41 +0100

    Create WorldBuilderClasses.py
    Added worldbuilderclasses containing classes used in worldbuilder

---
+++ WorldBuilderClasses.py
@@ -0,0 +1,17 @@
+import pygame as pg
+
+class text():
+    def __init__(self,text,x=0,y=0,size=50): 
+        font = pg.font.SysFont(None,size) # use default system font, size 10
+
+        self.text = text
+
+        self.surf = font.render(text,False,(0,0,255))
+        self.rect = self.surf.get_rect()
+
+        self.rect.x = x
+        self.rect.y = y
+
+        self.image = pg.sprite.Sprite() 
+        self.image.image = self.surf
+        self.image.rect = self.rect
